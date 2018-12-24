0d688d2619c1720822b95bd89395d47ff9f13ee7
naetdaw <41181845+naetdaw@users.noreply.github.com>
2018-12-24 15:40:16 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset ( $_SESSION['ses_userid'] );
-unset ( $_SESSION['ses_username'] );
-unset ( $_SESSION['ses_status'] );
-session_destroy();
-header("Location:login.php");
-die();
-?>
